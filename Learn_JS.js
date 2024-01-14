a0c65e7096d1b71eb5dcfd1f45a759fde0b56f8f
KhanhChung2k5 <122288517+KhanhChung2k5@users.noreply.github.com>
2024-01-14 20:38:13 +0700

    Update Learn_JS.js

--- Learn_JS.js
+++ Learn_JS.js
@@ -31,3 +31,38 @@ for (let i = 0; i < n; i++){
 isPrime(arr);
 let countSNT = countPrime(arr);
 console.log("So nguye to co trong mang la: ${count}");
+
+
+
+
+// tyopesccript 
+import * as readlineSync from 'readline-sync';
+function isPrime(number: number): boolean{
+    if (number < 2){
+        return false;
+    }
+    else{
+        for (let i = 2; i < Math.sqrt(number); i++){
+            if (number % i === 0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+function countPrime(arr: number[]): number {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        if(isPrime(arr[i])){
+            count++;
+        }
+    }
+    return count;
+}
+let n: number = parseInt(readlineSync.question("Nhap so phan tu co trong mang:"));
+const arr: number[] = [];
+for (let i = 0; i < n; i++){
+    arr.push(parseInt(readlineSync.question(Nhap phan tu thu ${i + 1}:)))
+}
+const countSNT: number = countPrime(arr);
+console.log(`>>> So nguyen to co trong mang la ${countSNT}`)
