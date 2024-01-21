1527baa52522d8742b9fe300bab0c63d680409a2
KhanhChung2k5 <122288517+KhanhChung2k5@users.noreply.github.com>
2024-01-21 21:36:04 +0700

    Update ThapHN.ts

---
+++ test/src/ThapHN.ts
@@ -0,0 +1,23 @@
+// ta co from la coc A, to la coc C, ohter la coc B ( coc trung gian)
+function solveHanoi(
+    count: number,
+    from: string,
+    to: string,
+    other: string,
+    move: (from: string, to: string) => void
+)
+{
+    if (count > 0){
+        // di chuyen n - 1 dia tren cung sang coc B thong qua cot C
+        solveHanoi(count - 1, from, other, to, move);
+        move(from, to);
+        // di chuyen dia con lai qua cot C
+        // di chuyen n-1 dia tu COT B sang cot C
+        solveHanoi(count - 1, other, from, to, move);
+    }
+}
+var movecount = 0;
+solveHanoi(6, "Left", "Right", "Middle", (from, to)=>{
+    ++movecount;
+    console.log(movecount + ": Move from " + from + " to " + to + ".")
+})
