b7e7217c435c8827b9165910cb98ad7a2b6eaef0
DESKTOP-49PPGNM\Lenovo <chtkkiet@gmail.com>
2024-01-21 19:53:46 +0700

    feat: add learn.js

--- Learn_JS.js
+++ Learn_JS.js
@@ -1,2 +1,32 @@
-// learn-js 
-console.log("learn-js");
+function isPrime(num){
+    if (num < 2){
+        return false;
+    }
+    else {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0){
+                return false;
+
+            }
+        }
+    }
+    return true;
+}
+function countPrime(arr){
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (isPrime(arr[i])){
+            count++;
+        }
+    }
+    return count;
+}
+let n = parseInt(prompt("Nhap vao so phan tu co trong mang: "));
+let arr =[];
+for (let i = 0; i < n; i++){
+    let num = parseInt(prompt("Nhap phan tu thu " + (i + 1) + ":"));
+    arr.push(arr[i]);
+}
+isPrime(arr);
+let countSNT = countPrime(arr);
+console.log("So nguye to co trong mang la: ${count}");
